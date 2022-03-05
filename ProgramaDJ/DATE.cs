4a8565fe5a791b79caee8e87d377626329e6daee
DennisMC20 <72049653+DennisMC20@users.noreply.github.com>
2022-03-04 20:52:38 -0600

    Version DMJ 001
    Practica de clase

---
+++ ProgramaDJ/DATE.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramaDJ
+{
+    internal class Date
+    {
+        private int _year;
+        private int _month;
+        private int _day;
+        //private int _hour;
+        //private int _minute;
+        //private int _second;
+        //private int _millisecond;
+
+
+        public Date(int year, int month, int day)
+        {
+            _year = year;
+            _month = month;
+            _day = day;
+
+        }
+        public override string ToString()
+        {
+            return _year + "/" + _month + "/" + _day;
+        }
+    }
+}
---
+++ ProgramaDJ/Program.cs
@@ -0,0 +1,7 @@
+﻿using ProgramaDJ;
+
+Console.WriteLine("Primera parte");
+Console.WriteLine("-------------");
+
+Date date1 = new Date(2022, 3, 4);
+Console.WriteLine(date1);
